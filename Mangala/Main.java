475f1a1393ba6138de7148cffcfe492493daf496
crestand <45559229+crestand@users.noreply.github.com>
2022-09-29 14:34:12 +0300

    Add files via upload

---
+++ Mangala/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Driver class
+ */
+public class Main {
+    public static void main( String[] args){
+        MangalaGameHandler mangalaGameHandler = new MangalaGameHandler();
+        mangalaGameHandler.gameLoop();
+    }
+}
